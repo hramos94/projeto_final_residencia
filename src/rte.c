78eb1f69cd109f42ab5184b5f99fe07fc455cdd7
andremgbr <andreroetger@yahoo.com.br>
2025-03-13 18:31:18 -0300

    feat:created a graphic user interface with 10 PINs

--- src/rte.c
+++ src/rte.c
@@ -1,6 +1,7 @@
 #include <app.h>
 #include <mcal.h>
 #include <ecu.h>
+#include <gui.h>
 
 int main(int argc, char *argv[]){
 
@@ -11,7 +12,10 @@ int main(int argc, char *argv[]){
   pthread_t hazard_th = new_thread(hazard_lights_blink);
   pthread_t read_input_th = new_thread(read_input);
 
+  pthread_t gui_runner_th = new_thread(gui_runner);
+
   pthread_join(hazard_th, NULL);
   pthread_join(read_input_th, NULL);
+  pthread_join(gui_runner_th, NULL);
 
 }

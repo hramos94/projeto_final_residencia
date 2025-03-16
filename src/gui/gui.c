67263486e6a13bbccaf974e9f0fd2e3bd3298383
andremgbr <andreroetger@yahoo.com.br>
2025-03-16 00:33:10 -0300

    doc:formated all files

--- src/gui/gui.c
+++ src/gui/gui.c
@@ -22,105 +22,115 @@
 #define MAX_VERTEX_BUFFER 512 * 1024
 #define MAX_ELEMENT_BUFFER 128 * 10240
 
-int gui_runner() {
-  struct nk_glfw glfw = {0};
-  static GLFWwindow *win;
-  struct nk_context *ctx;
-  struct nk_colorf bg = {0.10f, 0.18f, 0.24f, 1.0f};
+int gui_runner()
+{
+    struct nk_glfw glfw = {0};
+    static GLFWwindow *win;
+    struct nk_context *ctx;
+    struct nk_colorf bg = {0.10f, 0.18f, 0.24f, 1.0f};
 
+    /* Inicializa GLFW */
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Erro: Falha ao inicializar GLFW!\n");
+        return -1;
+    }
 
-  /* Inicializa GLFW */
-  if (!glfwInit()) {
-    fprintf(stderr, "Erro: Falha ao inicializar GLFW!\n");
-    return -1;
-  }
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-  win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "REB GUI", NULL, NULL);
-  if (!win) {
-    fprintf(stderr, "Erro: Falha ao criar janela!\n");
-    glfwTerminate();
-    return -1;
-  }
+    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "REB GUI", NULL, NULL);
+    if (!win)
+    {
+        fprintf(stderr, "Erro: Falha ao criar janela!\n");
+        glfwTerminate();
+        return -1;
+    }
 
-  glfwMakeContextCurrent(win);
-  glewExperimental = 1;
-  if (glewInit() != GLEW_OK) {
-    fprintf(stderr, "Erro: Falha ao inicializar GLEW!\n");
-    return -1;
-  }
+    glfwMakeContextCurrent(win);
+    glewExperimental = 1;
+    if (glewInit() != GLEW_OK)
+    {
+        fprintf(stderr, "Erro: Falha ao inicializar GLEW!\n");
+        return -1;
+    }
 
-  /* Inicializa Nuklear */
-  ctx = nk_glfw3_init(&glfw, win, NK_GLFW3_INSTALL_CALLBACKS);
-  struct nk_font_atlas *atlas;
-  nk_glfw3_font_stash_begin(&glfw, &atlas);
-  struct nk_font *font =
-      nk_font_atlas_add_default(atlas, 13, NULL); // Carregar uma fonte padrão
-  nk_glfw3_font_stash_end(&glfw);
+    /* Inicializa Nuklear */
+    ctx = nk_glfw3_init(&glfw, win, NK_GLFW3_INSTALL_CALLBACKS);
+    struct nk_font_atlas *atlas;
+    nk_glfw3_font_stash_begin(&glfw, &atlas);
+    struct nk_font *font = nk_font_atlas_add_default(atlas, 13, NULL); // Carregar uma fonte padrão
+    nk_glfw3_font_stash_end(&glfw);
 
-  nk_style_set_font(ctx, &font->handle);
+    nk_style_set_font(ctx, &font->handle);
 
-  static int button_state[10]; // Estado inicial de cada botão
-  for (int i = 0; i < 10; i++) {
-    button_state[i] = 0;
-  }
-  while (!glfwWindowShouldClose(win)) {
-    glfwPollEvents();
-    nk_glfw3_new_frame(&glfw);
+    static int button_state[10]; // Estado inicial de cada botão
+    for (int i = 0; i < 10; i++)
+    {
+        button_state[i] = 0;
+    }
+    while (!glfwWindowShouldClose(win))
+    {
+        glfwPollEvents();
+        nk_glfw3_new_frame(&glfw);
 
-    char buton_label[20];
+        char buton_label[20];
 
-    /* Interface */
-    if (nk_begin(ctx, "INPUT", nk_rect(50, 50, 230, 500),
-                 NK_WINDOW_TITLE | NK_WINDOW_NO_SCROLLBAR)) {
+        /* Interface */
+        if (nk_begin(ctx, "INPUT", nk_rect(50, 50, 230, 500),
+                     NK_WINDOW_TITLE | NK_WINDOW_NO_SCROLLBAR))
+        {
 
-      nk_layout_row_dynamic(ctx, 30, 1);
+            nk_layout_row_dynamic(ctx, 30, 1);
 
-      for (int i = 0; i < 10; i++) {
-        sprintf(buton_label, "PIN %d", i);
-        if (nk_button_label(ctx, buton_label)) {
-          button_state[i] = !button_state[i]; // Alterna estado
-          set_pin_status(button_state[i], i);
+            for (int i = 0; i < 10; i++)
+            {
+                sprintf(buton_label, "PIN %d", i);
+                if (nk_button_label(ctx, buton_label))
+                {
+                    button_state[i] = !button_state[i]; // Alterna estado
+                    set_pin_status(button_state[i], i);
+                }
+            }
         }
-      }
+        nk_end(ctx);
+
+        if (nk_begin(ctx, "OUTPUT", nk_rect(300, 50, 230, 500),
+                     NK_WINDOW_TITLE | NK_WINDOW_NO_SCROLLBAR))
+        {
+            nk_layout_row_dynamic(ctx, 30, 1);
+
+            uint8_t *status = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                read_pin_status(&status, i);
+                sprintf(buton_label, "PIN %d - %s", i, status ? "ON" : "OFF");
+                nk_label(ctx, buton_label, NK_TEXT_CENTERED);
+            }
+        }
+        nk_end(ctx);
+
+        /* Renderização */
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(bg.r, bg.g, bg.b, bg.a);
+        nk_glfw3_render(&glfw, NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER, MAX_ELEMENT_BUFFER);
+        glfwSwapBuffers(win);
     }
-    nk_end(ctx);
 
-    if (nk_begin(ctx, "OUTPUT", nk_rect(300, 50, 230, 500),
-                 NK_WINDOW_TITLE | NK_WINDOW_NO_SCROLLBAR)) {
-      nk_layout_row_dynamic(ctx, 30, 1);
-
-      uint8_t *status = 0;
-      for (int i = 0; i < 10; i++) {
-        read_pin_status(&status, i);
-        sprintf(buton_label, "PIN %d - %s", i, status ? "ON" : "OFF");
-        nk_label(ctx, buton_label, NK_TEXT_CENTERED);
-      }
+    /* Limpeza */
+    if (ctx)
+    {
+        nk_glfw3_shutdown(&glfw);
     }
-    nk_end(ctx);
 
-    /* Renderização */
-    glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(bg.r, bg.g, bg.b, bg.a);
-    nk_glfw3_render(&glfw, NK_ANTI_ALIASING_ON, MAX_VERTEX_BUFFER,
-                    MAX_ELEMENT_BUFFER);
-    glfwSwapBuffers(win);
-  }
+    if (win)
+    {
 
-  /* Limpeza */
-  if (ctx) {
-    nk_glfw3_shutdown(&glfw);
-  }
-
-  if (win) {
-
-    glfwDestroyWindow(win);
-  }
-  glfwTerminate();
-  return 0;
+        glfwDestroyWindow(win);
+    }
+    glfwTerminate();
+    return 0;
 }
--- src/mcal.c
+++ src/mcal.c
@@ -1,18 +1,17 @@
 #include <ecu.h>
 #include <mcal.h>
 
-#include <stdio.h>       // printf, perror, sprintf
-#include <stdint.h>      // uint8_t
-#include <string.h>      // strcpy, strncpy, memset
-#include <stdlib.h>      // system
-#include <unistd.h>      // close, write
-#include <sys/types.h>   // Tipos de dados do sistema
-#include <sys/socket.h>  // socket, bind
-#include <linux/can.h>   // struct can_frame, canid_t
+#include <linux/can.h>     // struct can_frame, canid_t
 #include <linux/can/raw.h> // CAN_RAW para socket
-#include <net/if.h>      // struct ifreq, IFNAMSIZ
-#include <sys/ioctl.h>   // ioctl, SIOCGIFFLAGS, SIOCGIFINDEX
-
+#include <net/if.h>        // struct ifreq, IFNAMSIZ
+#include <stdint.h>        // uint8_t
+#include <stdio.h>         // printf, perror, sprintf
+#include <stdlib.h>        // system
+#include <string.h>        // strcpy, strncpy, memset
+#include <sys/ioctl.h>     // ioctl, SIOCGIFFLAGS, SIOCGIFINDEX
+#include <sys/socket.h>    // socket, bind
+#include <sys/types.h>     // Tipos de dados do sistema
+#include <unistd.h>        // close, write
 
 dIO pins[10];
 // pin 0 - luz de pisca alerta
@@ -22,71 +21,75 @@ dIO pins[10];
 // pin 4 - Server envia desbloqueio REB
 // pin 5 - REB envia sinal para motor ser bloqueado
 
-pthread_t new_thread(void *func){
-  pthread_t nthread;
-  pthread_create(&nthread, NULL, func, NULL);  
-  return nthread;
+pthread_t new_thread(void *func)
+{
+    pthread_t nthread;
+    pthread_create(&nthread, NULL, func, NULL);
+    return nthread;
 }
 
-void show_error(char errorStr[]){
-  printf("%s", errorStr);
-}
+void show_error(char errorStr[]) { printf("%s", errorStr); }
 
-uint8_t read_pint_status(uint8_t *p_pin, uint8_t *p_status){
-  char *line = NULL;
-  size_t len = 0;
-  if(getline(&line, &len, stdin) == -1){
-    return FAIL;
-  }
-  if (memcmp(line, "pin", 3) == 0) {
-    int pin,  status;
-    if(sscanf(line +4, "%d %d", &pin, &status) == 2){
-      if(status == 0 || status == 1){
-        *p_pin = pin;
-        *p_status = status;
-        return SUCCESS;
-      }
+uint8_t read_pint_status(uint8_t *p_pin, uint8_t *p_status)
+{
+    char *line = NULL;
+    size_t len = 0;
+    if (getline(&line, &len, stdin) == -1)
+    {
+        return FAIL;
     }
-
-  }
-  return FAIL;
+    if (memcmp(line, "pin", 3) == 0)
+    {
+        int pin, status;
+        if (sscanf(line + 4, "%d %d", &pin, &status) == 2)
+        {
+            if (status == 0 || status == 1)
+            {
+                *p_pin = pin;
+                *p_status = status;
+                return SUCCESS;
+            }
+        }
+    }
+    return FAIL;
 }
 
-uint8_t mcal_init(){
+uint8_t mcal_init()
+{
 
-  pins[0].pinNumber = 0;
-  pins[0].status = 0;
+    pins[0].pinNumber = 0;
+    pins[0].status = 0;
 
-  pins[1].pinNumber = 1;
-  pins[1].status = 0;
+    pins[1].pinNumber = 1;
+    pins[1].status = 0;
 
-  return SUCCESS;
+    return SUCCESS;
 }
 
-uint8_t read_pin_status(uint8_t *status, uint8_t pin){
-  *status = pins[pin].status; 
+uint8_t read_pin_status(uint8_t *status, uint8_t pin)
+{
+    *status = pins[pin].status;
 
-  return SUCCESS;
+    return SUCCESS;
 }
 
-uint8_t set_pin_status(uint8_t p_status, uint8_t p_pin){
-  pins[p_pin].status = p_status;
-  printf(">>> Set pin%d = %d\n",p_pin, p_status);
-  return SUCCESS;
+uint8_t set_pin_status(uint8_t p_status, uint8_t p_pin)
+{
+    pins[p_pin].status = p_status;
+    printf(">>> Set pin%d = %d\n", p_pin, p_status);
+    return SUCCESS;
 }
 
-void go_sleep(uint8_t seconds){
-  sleep(seconds);
-}
+void go_sleep(uint8_t seconds) { sleep(seconds); }
 
 //======================================================================
 //======================== CAN =========================================
 //======================================================================
 
-//Opens Can socket - does not depend on VCAN interface
+// Opens Can socket - does not depend on VCAN interface
 uint8_t can_socket_open(int *can_socket)
 {
-    if ((*can_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) 
+    if ((*can_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
     {
         perror("Socket Open Failed: ");
         return FAIL;
@@ -94,10 +97,10 @@ uint8_t can_socket_open(int *can_socket)
     return SUCCESS;
 }
 
-//Closes CAN socket - does not depend on VCAN interface
+// Closes CAN socket - does not depend on VCAN interface
 uint8_t can_socket_close(int *can_socket)
 {
-    if (close(*can_socket) < 0) 
+    if (close(*can_socket) < 0)
     {
         perror("Socket Close Failed:");
         return FAIL;
@@ -105,18 +108,18 @@ uint8_t can_socket_close(int *can_socket)
     return SUCCESS;
 }
 
-// Function to verify if can socket exists 
+// Function to verify if can socket exists
 uint8_t can_interface_verify(const char *interface)
 {
     char verify_can_command[100];
-    sprintf(verify_can_command,"ip link show %s > /dev/null 2>&1", interface);
+    sprintf(verify_can_command, "ip link show %s > /dev/null 2>&1", interface);
 
-    //return FAIL if interface doesn not exists
-    if(system(verify_can_command) != 0) 
+    // return FAIL if interface doesn not exists
+    if (system(verify_can_command) != 0)
     {
-      return FAIL;
+        return FAIL;
     }
-    return SUCCESS; 
+    return SUCCESS;
 }
 
 // Function to verify if can interface is UP or DOWN
@@ -124,27 +127,27 @@ uint8_t can_interface_status(int *can_socket, const char *interface)
 {
     struct ifreq socket_info; // Initialize the struct ifreq to hold the interface information
     strncpy(socket_info.ifr_name, interface, IFNAMSIZ);
-    
-    // check if the interface exist getting the status using ioctl 
-    if (ioctl(*can_socket, SIOCGIFFLAGS, &socket_info) < 0) 
+
+    // check if the interface exist getting the status using ioctl
+    if (ioctl(*can_socket, SIOCGIFFLAGS, &socket_info) < 0)
     {
         perror("Error getting interface flags");
         return FAIL;
     }
-    
+
     // Check if the interface is up - IFF_UP is a flag that indicate if interface is UP
     if (socket_info.ifr_flags & IFF_UP)
     {
         return SUCCESS;
-    } 
-    else 
+    }
+    else
     {
-        return FAIL;  
+        return FAIL;
     }
 }
 
-//Armed with the interface index, we can now bind the socket to the CAN Interface:
-uint8_t can_bind_socket(int *can_socket,const char *interface)
+// Armed with the interface index, we can now bind the socket to the CAN Interface:
+uint8_t can_bind_socket(int *can_socket, const char *interface)
 {
     struct ifreq ifr;
     strcpy(ifr.ifr_name, interface);
@@ -154,19 +157,18 @@ uint8_t can_bind_socket(int *can_socket,const char *interface)
     memset(&addr, 0, sizeof(addr));
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
-    if (bind(*can_socket, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+    if (bind(*can_socket, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         perror("Bind");
         return FAIL;
     }
     return SUCCESS;
-
 }
 
-//function to send frames on CAN BUS
+// function to send frames on CAN BUS
 uint8_t can_send(int *can_socket, struct can_frame *frame)
 {
-    if (write(*can_socket, frame, sizeof(struct can_frame)) != sizeof(struct can_frame)) 
+    if (write(*can_socket, frame, sizeof(struct can_frame)) != sizeof(struct can_frame))
     {
         perror("Can Write error: ");
         return FAIL;
@@ -174,12 +176,12 @@ uint8_t can_send(int *can_socket, struct can_frame *frame)
     return SUCCESS;
 }
 
-//function to read frames on CAN - this will block execution until a frame is received
+// function to read frames on CAN - this will block execution until a frame is received
 uint8_t can_read(int *can_socket, struct can_frame *frame)
 {
-    //this will block until frame avaliable
-    int nbytes = read(*can_socket, frame, sizeof(struct can_frame)); 
-    if (nbytes < 0) 
+    // this will block until frame avaliable
+    int nbytes = read(*can_socket, frame, sizeof(struct can_frame));
+    if (nbytes < 0)
     {
         perror("Can Read Error: ");
         return FAIL;
@@ -189,31 +191,31 @@ uint8_t can_read(int *can_socket, struct can_frame *frame)
 
 uint8_t can_start(int *my_vcan, const char *interface)
 {
-    if(can_socket_open(my_vcan)==FAIL)
+    if (can_socket_open(my_vcan) == FAIL)
     {
         return FAIL;
     }
-    if(can_interface_status(my_vcan,interface)==FAIL)
+    if (can_interface_status(my_vcan, interface) == FAIL)
     {
         return FAIL;
     }
-    if(can_bind_socket(my_vcan,interface)==FAIL)
+    if (can_bind_socket(my_vcan, interface) == FAIL)
     {
         return FAIL;
     }
     return SUCCESS;
 }
 
-//send can frame in vcan0
+// send can frame in vcan0
 uint8_t can_send_vcan0(struct can_frame *frame)
 {
     int my_vcan;
     const char *interface = "vcan0";
-    if(can_start(&my_vcan,interface)==FAIL)
+    if (can_start(&my_vcan, interface) == FAIL)
     {
         return FAIL;
     }
-    if(can_send(&my_vcan,frame)==FAIL)
+    if (can_send(&my_vcan, frame) == FAIL)
     {
         return FAIL;
     }
@@ -221,27 +223,19 @@ uint8_t can_send_vcan0(struct can_frame *frame)
     return SUCCESS;
 }
 
-//read frame in vcan0 - will block until frame is received
+// read frame in vcan0 - will block until frame is received
 uint8_t can_read_vcan0(struct can_frame *frame)
 {
     int my_vcan;
     const char *interface = "vcan0";
-    if(can_start(&my_vcan,interface)==FAIL)
+    if (can_start(&my_vcan, interface) == FAIL)
     {
         return FAIL;
     }
-    if(can_read(&my_vcan,frame)==FAIL)
+    if (can_read(&my_vcan, frame) == FAIL)
     {
         return FAIL;
     }
     can_socket_close(&my_vcan);
     return SUCCESS;
 }
-
-
-
-
-
-
-
-

559c3e21292fb2b80ef8ecd2470a3740434aad4b
DennisIrineu <dennispaulinoirineu@gmail.com>
2025-03-15 22:26:52 -0300

    feat: initial creation of start reb and initiation start reb

--- src/app.c
+++ src/app.c
@@ -2,56 +2,85 @@
 #include <ecu.h>
 #include <mcal.h>
 
-uint8_t application_init() {
+uint8_t application_init()
+{
 
-  if (mcal_init() == FAIL) {
-    show_error("mcal_init FAIL\n");
-  }
+    if (mcal_init() == FAIL)
+    {
+        show_error("mcal_init FAIL\n");
+    }
 
-  return SUCCESS;
+    return SUCCESS;
 }
 
-uint8_t read_input(){
-  while(1){
-    if(read_console() == FAIL){
-      show_error("app.read_console FAIL\n");
+uint8_t read_input()
+{
+    while (1)
+    {
+        if (read_console() == FAIL)
+        {
+            show_error("app.read_console FAIL\n");
+        }
     }
-  }
 }
 
-uint8_t hazard_lights_blink() {
-  while (1) {
-    uint8_t status = 0;
-    if(get_hazard_button_status(&status) == FAIL){
-      show_error("get_hazard_button_status FAIL\n");
-      return FAIL;
+uint8_t hazard_lights_blink()
+{
+    while (1)
+    {
+        uint8_t status = 0;
+        if (get_hazard_button_status(&status) == FAIL)
+        {
+            show_error("get_hazard_button_status FAIL\n");
+            return FAIL;
+        }
+        if (status == S_ON)
+        {
+            if (set_hazard_light(S_ON) == FAIL)
+            {
+                show_error("set_hazard_light FAIL\n");
+                return FAIL;
+            };
+            go_sleep(1);
+            if (set_hazard_light(S_OFF) == FAIL)
+            {
+                show_error("set_hazard_light FAIL\n");
+                return FAIL;
+            };
+            go_sleep(1);
+        }
+        else
+        {
+            go_sleep(2);
+        }
     }
-    if (status == S_ON) {
-      if (set_hazard_light(S_ON) == FAIL){
-        show_error("set_hazard_light FAIL\n");
-        return FAIL;
-      };
-      go_sleep(1);
-      if (set_hazard_light(S_OFF) == FAIL){
-        show_error("set_hazard_light FAIL\n");
-        return FAIL;
-      };
-      go_sleep(1);
-    }else{
-      go_sleep(2);
-    }
-  }
 }
 
-uint8_t monitor_engine_block() {
-  while (1) {
-    if (block_engine() == FAIL){
-      show_error("app.block_engine FAIL\n");
+uint8_t monitor_engine_block()
+{
+    while (1)
+    {
+        if (block_engine() == FAIL)
+        {
+            show_error("app.block_engine FAIL\n");
+        }
+        go_sleep(1);
+        if (unblock_engine() == FAIL)
+        {
+            show_error("app.unblock_engine FAIL\n");
+        }
+        go_sleep(1);
     }
-    go_sleep(1);
-    if (unblock_engine() == FAIL) {
-      show_error("app.unblock_engine FAIL\n");
+}
+
+uint8_t initiation_start_reb()
+{
+    while (1)
+    {
+        if (start_reb() == FAIL)
+        {
+            show_error("app.start_reb FAIL\n");
+        }
+        go_sleep(1);
     }
-    go_sleep(1);
-  }
 }
--- src/ecu.c
+++ src/ecu.c
@@ -1,107 +1,174 @@
 #include <ecu.h>
 #include <mcal.h>
+#include <time.h>
+#include <unistd.h>
 
-uint8_t get_hazard_button_status(uint8_t *status){
-  if(read_pin_status(status, 1) == FAIL){
-    show_error("ECU.read_pin_status FAIL\n");
-    return FAIL;
-  }
-  return SUCCESS;
-} 
-
-uint8_t set_hazard_light(uint8_t status){
-  if(set_pin_status(status, 0)==FAIL){
-    show_error("ECU.set_pin_status FAIL\n");
-    return FAIL;
-  }
-  return SUCCESS;
+uint8_t get_hazard_button_status(uint8_t *status)
+{
+    if (read_pin_status(status, 1) == FAIL)
+    {
+        show_error("ECU.read_pin_status FAIL\n");
+        return FAIL;
+    }
+    return SUCCESS;
 }
 
-uint8_t read_console(){
-  uint8_t pin = 0, status = 0;
-  if(read_pint_status(&pin, &status) == FAIL){
-    show_error("read_pin_status FAIL\n");
-    return FAIL;
-  }
-
-  if(set_pin_status(status, pin)== FAIL){
-    show_error("read_console.set_pin_status FAIL\n");
-    return FAIL;
-  }
-
-  return SUCCESS;
+uint8_t set_hazard_light(uint8_t status)
+{
+    if (set_pin_status(status, 0) == FAIL)
+    {
+        show_error("ECU.set_pin_status FAIL\n");
+        return FAIL;
+    }
+    return SUCCESS;
 }
 
-uint8_t block_engine(){
-  uint8_t status = 0;
-
-  // check server status (pin 2)
-  if(read_pin_status(&status, 2) == FAIL){
-    show_error("ECU.read_pin_status (server block request)\n");
-    return FAIL;
-  }
-
-  if(status  == S_ON) {
-
-    // change pin 4 to off (unblock request)
-    if (set_pin_status(S_OFF, 4) == FAIL) {
-      show_error("ECU.set_pin_status FAIL (turn off unblock server request)\n");
-      return FAIL;
-    }
-
-    // Send command ON to pin 5 (block engine)
-    if(set_pin_status(S_ON, 5) == FAIL){
-      show_error("ECU.set_pin_status FAIL (engine block)\n");
-      return FAIL;
-    }
-
-    // Send command ON to pin 3 (driver notification)
-    if(set_pin_status(S_ON, 3) == FAIL){
-      show_error("ECU.set_pin_status FAIL (driver notification)\n");
-      return FAIL;
-    }
-  }
-
-  return SUCCESS;
-}
-
-uint8_t unblock_engine(){
-  uint8_t status = 0;
-
-  // Check server status (pin 4)
-  if (read_pin_status(&status, 4) == FAIL) {
-      show_error("ECU.read_pin_status FAIL (server unblock request)\n");
-      return FAIL;
-  }
-  if (status == S_ON) {
-
-    // change pin 2 to off
-    if (set_pin_status(S_OFF, 2) == FAIL) {
-      show_error("ECU.set_pin_status FAIL (turn off block request)\n");
-      return FAIL;
-    }
-    // Send unblock engine command (pin 5)
-    if (set_pin_status(S_OFF, 5) == FAIL) {
-        show_error("ECU.set_pin_status FAIL (engine unblock)\n");
+uint8_t read_console()
+{
+    uint8_t pin = 0, status = 0;
+    if (read_pint_status(&pin, &status) == FAIL)
+    {
+        show_error("read_pin_status FAIL\n");
         return FAIL;
     }
 
-    // Remove driver notification (pin 3)
-    if (set_pin_status(S_OFF, 3) == FAIL) {
-        show_error("ECU.set_pin_status FAIL (remove driver notification)\n");
+    if (set_pin_status(status, pin) == FAIL)
+    {
+        show_error("read_console.set_pin_status FAIL\n");
         return FAIL;
     }
-  }
 
-  return SUCCESS;
+    return SUCCESS;
 }
 
-uint8_t engine_block_status(uint8_t *status){
-  // read engine status (block=1 or unblocked=0)
-  if(read_pin_status(status, 5) == FAIL) {
-    show_error("ECU.read_pin_status FAIL (get engine status)\n");
-    return FAIL;
-  }
+uint8_t start_reb()
+{
+    uint8_t status = 0;
+    struct timespec start_time, current_time;
+    const int countdown_sec = 5;
 
-  return SUCCESS;
+    if (read_pin_status(&status, 3) == FAIL)
+    {
+        show_error("ECU.read_pin_status FAIL (get start_reb status)\n");
+        return FAIL;
+    }
+
+    if (status == S_ON)
+    {
+        clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+        do
+        {
+            if (read_pin_status(&status, 3) == FAIL)
+            {
+                show_error("ECU.read_pin_status FAIL (during count)\n");
+                return FAIL;
+            }
+
+            if (status == S_OFF)
+            {
+                return SUCCESS;
+            }
+
+            clock_gettime(CLOCK_MONOTONIC, &current_time);
+            double elapsed_time = (current_time.tv_sec - start_time.tv_sec) +
+                                  (current_time.tv_nsec - start_time.tv_nsec) / 1.0e9;
+
+            if (elapsed_time >= countdown_sec)
+            {
+                return block_engine();
+            }
+
+        } while (1);
+    }
+
+    return SUCCESS;
+}
+
+uint8_t block_engine()
+{
+    uint8_t status = 0;
+
+    // check server status (pin 2)
+    if (read_pin_status(&status, 2) == FAIL)
+    {
+        show_error("ECU.read_pin_status (server block request)\n");
+        return FAIL;
+    }
+
+    if (status == S_ON)
+    {
+
+        // change pin 4 to off (unblock request)
+        if (set_pin_status(S_OFF, 4) == FAIL)
+        {
+            show_error("ECU.set_pin_status FAIL (turn off unblock server request)\n");
+            return FAIL;
+        }
+
+        // Send command ON to pin 5 (block engine)
+        if (set_pin_status(S_ON, 5) == FAIL)
+        {
+            show_error("ECU.set_pin_status FAIL (engine block)\n");
+            return FAIL;
+        }
+
+        // Send command ON to pin 3 (driver notification)
+        if (set_pin_status(S_ON, 3) == FAIL)
+        {
+            show_error("ECU.set_pin_status FAIL (driver notification)\n");
+            return FAIL;
+        }
+    }
+
+    return SUCCESS;
+}
+
+uint8_t unblock_engine()
+{
+    uint8_t status = 0;
+
+    // Check server status (pin 4)
+    if (read_pin_status(&status, 4) == FAIL)
+    {
+        show_error("ECU.read_pin_status FAIL (server unblock request)\n");
+        return FAIL;
+    }
+    if (status == S_ON)
+    {
+
+        // change pin 2 to off
+        if (set_pin_status(S_OFF, 2) == FAIL)
+        {
+            show_error("ECU.set_pin_status FAIL (turn off block request)\n");
+            return FAIL;
+        }
+        // Send unblock engine command (pin 5)
+        if (set_pin_status(S_OFF, 5) == FAIL)
+        {
+            show_error("ECU.set_pin_status FAIL (engine unblock)\n");
+            return FAIL;
+        }
+
+        // Remove driver notification (pin 3)
+        if (set_pin_status(S_OFF, 3) == FAIL)
+        {
+            show_error("ECU.set_pin_status FAIL (remove driver notification)\n");
+            return FAIL;
+        }
+    }
+
+    return SUCCESS;
+}
+
+uint8_t engine_block_status(uint8_t *status)
+{
+    // read engine status (block=1 or unblocked=0)
+    if (read_pin_status(status, 5) == FAIL)
+    {
+        show_error("ECU.read_pin_status FAIL (get engine status)\n");
+        return FAIL;
+    }
+
+    return SUCCESS;
 }

a9a1b910880a2650fb5854d7c10d207bd1545e5f
TerenceKutzner <terence.kutzner@hotmail.com>
2025-03-25 13:10:05 +0000

    feat: Performance Improvements

--- aux/src/gui/ipc.c
+++ aux/src/gui/ipc.c
@@ -14,7 +14,7 @@
 const uint32_t FPS = 30;
 
 #ifndef M_PI
-    #define M_PI 3.14159265358979323846
+    #define M_PI 3.141592
 #endif
 
 uint8_t reb_fault_warning = 0;
@@ -24,9 +24,12 @@ uint8_t reb_imobilize_procedure = 0;
 uint8_t reb_vehicle_imobilized = 0;
 uint8_t accelerator_percentage=0;
 uint8_t brake_percentage=0;
+uint8_t hazard_lights_state=0;
+uint8_t hazard_light=0;
 float vehicle_speed_float=0;
 
 char terminal_frame_str[6][150]; // Buffer para armazenar a string
+char timestamp[6][11]; 
 
 int16_t terminal_read_can()
 {
@@ -35,7 +38,8 @@ int16_t terminal_read_can()
     uint8_t status = 0;
     for (int i = 0; i <= 6; i++) 
     {
-        terminal_frame_str[i][0] = ' ';  
+        terminal_frame_str[i][0] = ' ';
+        timestamp[i][0] = ' '; 
     }
 
     if (can_socket_open(&my_vcan_2) == FAIL)
@@ -85,7 +89,7 @@ int16_t terminal_read_can()
 
         //int offset = snprintf(frame_str, sizeof(frame_str), " [0x%X] ", frame.can_id, frame.can_dlc);
 
-        // Adiciona os dados ao buffer
+        // Adiciona os dados ao timestamp
         for (int i = 0; i < frame.can_dlc && offset < sizeof(frame_str) - 3; i++) {
             offset += snprintf(frame_str + offset, sizeof(frame_str) - offset, "%02X ", frame.data[i]);
         }
@@ -93,6 +97,9 @@ int16_t terminal_read_can()
         if(line_counter<6)
         {
             strcpy(terminal_frame_str[line_counter], frame_str);
+
+            strncpy(timestamp[line_counter], terminal_frame_str[line_counter], 10);  
+            timestamp[line_counter][10] = '\0'; 
             line_counter++;
         }
         else
@@ -100,14 +107,15 @@ int16_t terminal_read_can()
             for(int i=0;i<5;i++)
             {
                 strcpy(terminal_frame_str[i],terminal_frame_str[i+1]);
+
+                strncpy(timestamp[i], terminal_frame_str[i], 10);  
+                timestamp[i][10] = '\0'; 
             }
             strcpy(terminal_frame_str[5], frame_str);
+            strncpy(timestamp[5], terminal_frame_str[5], 10);  
+            timestamp[5][10] = '\0'; 
         }
         
-    
-        // Imprime a string formatada
-        printf("%s\n", frame_str);
-
     }
 }
 
@@ -153,17 +161,7 @@ int16_t ipc_runner()
 
 
     // Start thread for reading CAN for mini terminal
-
     pthread_t terminal_read_can_th = new_thread(terminal_read_can);
-
-
-    // Loop principal -- Responsável por fazer o movimento das coisas
-    SDL_Event e;
-    int16_t quit = 0;
-    int16_t frame_counter = 1;
-    int16_t counter2 =0;
-    uint8_t hazard_lights_state=0;
-    uint8_t hazard_light=0;
 
     //Vehicle Control Buttons
     const int16_t button1_x0 = 60;
@@ -214,6 +212,8 @@ int16_t ipc_runner()
     SDL_Color button_not_clicked = {110, 110, 110, 110};
     SDL_Color terminal_color = {48, 10, 36, 255};
 
+    SDL_Event e;
+    int16_t quit = 0;
 
     while (!quit) {
         while (SDL_PollEvent(&e)) {
@@ -231,6 +231,85 @@ int16_t ipc_runner()
                 }
                 handle_pedal_press(&accelarator, mouseX, mouseY,&accelerator_percentage,10);
                 handle_pedal_press(&brake, mouseX, mouseY,&brake_percentage,10);
+
+                //Ignition ON Button
+                if(buttons1[0].clicked == 1 && buttons1[0].last_click == 0)
+                {
+                    //set_pin_status(1, PIN);
+                    buttons1[0].last_click = 1;
+                    
+                }
+                else if(buttons1[0].clicked == 0 && buttons1[0].last_click == 1)
+                {
+                    //set_pin_status(0, PIN);
+                    buttons1[0].last_click = 0;
+                }
+
+                //REB ON Button
+                if(buttons1[1].clicked == 1 && buttons1[1].last_click == 0)
+                {
+                    set_pin_status(1, REB_ACTIVATE_PIN);
+                    buttons1[1].last_click = 1;
+                    
+                }
+                else if(buttons1[1].clicked == 0 && buttons1[1].last_click == 1)
+                {
+                    set_pin_status(0, REB_ACTIVATE_PIN);
+                    buttons1[1].last_click = 0;
+                }
+
+                //REB OFF Button
+                if(buttons1[2].clicked == 1 && buttons1[2].last_click == 0)
+                {
+                    set_pin_status(1, REB_DEACTIVATE);
+                    buttons1[2].last_click = 1;
+                    
+                }
+                else if(buttons1[2].clicked == 0 && buttons1[2].last_click == 1)
+                {
+                    set_pin_status(0, REB_DEACTIVATE);
+                    buttons1[2].last_click = 0;
+                }
+
+                //Scenario 1 Button
+                if(buttons1[3].clicked == 1 && buttons1[3].last_click == 0)
+                {
+                    //set_pin_status(1, PIN);
+                    buttons1[3].last_click = 1;
+                    
+                }
+                else if(buttons1[3].clicked == 0 && buttons1[3].last_click == 1)
+                {
+                    //set_pin_status(0, PIN);
+                    buttons1[3].last_click = 0;
+                }
+
+                //Scenario 2 Button
+                if(buttons1[4].clicked == 1 && buttons1[4].last_click == 0)
+                {
+                    //set_pin_status(1, PIN);
+                    buttons1[4].last_click = 1;
+                    
+                }
+                else if(buttons1[4].clicked == 0 && buttons1[4].last_click == 1)
+                {
+                    //set_pin_status(0, PIN);
+                    buttons1[4].last_click = 0;
+                }
+
+                //Scenario 3 Button
+                if(buttons1[5].clicked == 1 && buttons1[5].last_click == 0)
+                {
+                    //set_pin_status(1, PIN);
+                    buttons1[5].last_click = 1;
+                    
+                }
+                else if(buttons1[5].clicked == 0 && buttons1[5].last_click == 1)
+                {
+                    //set_pin_status(0, PIN);
+                    buttons1[5].last_click = 0;
+                }
+
             }
         }
 
@@ -259,7 +338,6 @@ int16_t ipc_runner()
         char tit4[50];
         snprintf(tit4, sizeof(tit4), "CAN Comunications");
         draw_text(renderer, font3, tit4, 952,680,white);
-
 
         //Speedometer and speed simulations
         if(reb_vehicle_imobilized == 1)
@@ -298,44 +376,7 @@ int16_t ipc_runner()
             draw_button(renderer, &buttons1[i], buttonColor, font3);
         }
 
-        //Ignition ON Button
-        if(buttons1[0].clicked == 1 && buttons1[0].last_click == 0)
-        {
-            //set_pin_status(1, PIN);
-            buttons1[0].last_click = 1;
-            
-        }
-        else if(buttons1[0].clicked == 0 && buttons1[0].last_click == 1)
-        {
-            //set_pin_status(0, PIN);
-            buttons1[0].last_click = 0;
-        }
-
-        //REB ON Button
-        if(buttons1[1].clicked == 1 && buttons1[1].last_click == 0)
-        {
-            set_pin_status(1, REB_ACTIVATE_PIN);
-            buttons1[1].last_click = 1;
-           
-        }
-        else if(buttons1[1].clicked == 0 && buttons1[1].last_click == 1)
-        {
-            set_pin_status(0, REB_ACTIVATE_PIN);
-            buttons1[1].last_click = 0;
-        }
-
-        //REB OFF Button
-        if(buttons1[2].clicked == 1 && buttons1[2].last_click == 0)
-        {
-            set_pin_status(1, REB_DEACTIVATE);
-            buttons1[2].last_click = 1;
-           
-        }
-        else if(buttons1[2].clicked == 0 && buttons1[2].last_click == 1)
-        {
-            set_pin_status(0, REB_DEACTIVATE);
-            buttons1[2].last_click = 0;
-        }
+    
 
 
         //Pins Reading
@@ -347,10 +388,10 @@ int16_t ipc_runner()
         
         if(reb_fault_warning==1)
         {
-            //alerta na parte superior
+            //Upper Alert
             draw_image(renderer, "./aux/img/reb_red.png", 480,284,408/7,227/7);
 
-            char velocidade[50]; //alerta de fault no lugar da velocidade
+            char velocidade[50]; //message in place of speed
             snprintf(velocidade, sizeof(velocidade), "REB Fault");
             draw_text(renderer, font2, velocidade, 600,420,white);
         }
@@ -380,17 +421,17 @@ int16_t ipc_runner()
         else
         {
 
-            char velocidade[10]; //texto da velocidade
+            char velocidade[10]; //speed text
             snprintf(velocidade, sizeof(velocidade), "%d", vehicle_speed);
             draw_text(renderer, font, velocidade, 600,400,white);
 
-            char km_indicator[10]; //texto da km
+            char km_indicator[10]; //km/h text
             snprintf(km_indicator, sizeof(km_indicator), "km/h");
             draw_text(renderer, font2,km_indicator, 600,440,white);
         
         }
     
-        if(hazard_lights_state==1) //pisca alerta
+        if(hazard_lights_state==1) //Hazard lights
         {
             draw_image(renderer, "./aux/img/hazard_warning.png", 685,284,92/2.5,85/2.5);
             if(hazard_light==1)
@@ -403,19 +444,15 @@ int16_t ipc_runner()
         //draw_image(renderer, "./aux/img/reb_green.png", 480,284,408/7,227/7);
 
 
-        char buffer[11];  // Buffer para armazenar os primeiros 10 caracteres + '\0'
+        //terminal text update
         for(uint8_t i=0;i<6;i++)
         {
+            //data in white
             draw_text_top_right(renderer, font4,terminal_frame_str[i], 750,740+40*i,white);
-            strncpy(buffer, terminal_frame_str[i], 10);  // Copia apenas os primeiros 10 caracteres
-            buffer[10] = '\0';  // Garante terminação da string
-        
-            draw_text_top_right(renderer, font4, buffer, 750, 740 + 40 * i, yellow);
+            
+            //timestamp in yellow
+            draw_text_top_right(renderer, font4, timestamp[i], 750, 740 + 40 * i, yellow); 
         }
-
-        
-        frame_counter = (frame_counter % 10000) + 1; // Contador de 1 a 100
-        counter2 = (counter2 % 200) + 1; // Contador de 1 a 100
 
         SDL_RenderPresent(renderer); // Atualizar a tela
         SDL_Delay((1/FPS)*1000); // Define o FPS 100

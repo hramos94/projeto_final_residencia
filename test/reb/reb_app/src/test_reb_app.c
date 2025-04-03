eb63204f3019b02ac357aab5c56b6b0e0225ab82
DennisIrineu <dennispaulinoirineu@gmail.com>
2025-04-03 14:29:29 -0300

    test: Include read_console_pin (with no warnings)

--- test/reb/reb_app/src/test_reb_app.c
+++ test/reb/reb_app/src/test_reb_app.c
@@ -11,14 +11,22 @@
 #include "app.h"
 #include "pthread.h"
 #include "string.h"
+#include "mcal.h"
+#include <unistd.h>
 
 extern int mock_can_write_return;
 
-TEST_GROUP(reb_app);
+// Declaração da função read_input que já existe
+extern uint8_t read_input();  
 
-// sometimes you may want to get at local data in a module.
-// for example: If you plan to pass by reference, this could be useful
-// however, it should often be avoided
+// Wrapper que chama read_input() e retorna NULL para compatibilidade com pthread_create
+void* read_input_wrapper(void* arg)
+{
+    read_input();  // Chama sua função original
+    return NULL;   // Retorna NULL para atender à assinatura esperada por pthread_create
+}
+
+TEST_GROUP(reb_app);
 
 TEST_SETUP(reb_app)
 {
@@ -30,15 +38,16 @@ TEST_TEAR_DOWN(reb_app) {}
 TEST(reb_app, test_read_console_pin_01)
 {
     pthread_t read_input_th;
-    pthread_create(&read_input_th, NULL, read_input, NULL);
+
+    // Agora chamamos o wrapper em vez de read_input diretamente
+    pthread_create(&read_input_th, NULL, read_input_wrapper, NULL);  
     pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
     int pipefd[2];
     pipe(pipefd);
 
-
     FILE *original_stdin = stdin;
     stdin = fdopen(pipefd[0], "r");
-
 
     char input_data[] = "pin 1 1\n";
     write(pipefd[1], input_data, strlen(input_data));
@@ -48,15 +57,11 @@ TEST(reb_app, test_read_console_pin_01)
 
     uint8_t result = read_pin_status(&status , 1);
 
-
-
     stdin = original_stdin;
     pthread_cancel(read_input_th);
 
     close(pipefd[1]);
 
-
     TEST_ASSERT_EQUAL(1, status);
     TEST_ASSERT_EQUAL(0, result);
-
 }

209855344b8cad78376c15473f62858bab6cf988
andremgbr <andreroetger@yahoo.com.br>
2025-04-28 11:00:50 -0300

    doc:file_utils.c misra compliance

--- bsw/src/file_utils.c
+++ bsw/src/file_utils.c
@@ -1,5 +1,8 @@
+#include <file_utils.h>
 #include <stdarg.h>
+// cppcheck-suppress misra-c2012-21.6
 #include <stdio.h>
+// cppcheck-suppress misra-c2012-21.10
 #include <time.h>
 
 FILE *file_fopen(const char *filename, const char *modes) { return fopen(filename, modes); }
@@ -10,9 +13,12 @@ int file_fclose(FILE *stream) { return fclose(stream); }
 
 int file_fprintf(FILE *stream, const char *format, ...)
 {
+    // cppcheck-suppress misra-c2012-17.1
     va_list args;
+    // cppcheck-suppress misra-c2012-17.1
     va_start(args, format);
     int result = vfprintf(stream, format, args);
+    // cppcheck-suppress misra-c2012-17.1
     va_end(args);
     return result;
 }
